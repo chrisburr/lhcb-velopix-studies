99d0ca99e66019a1b81407d8fbee70e9368d8c74
Christopher Burr <christopher.burr@cern.ch>
2017-02-20 16:42:41 +0100

    Move to using TrackIPResolutionChecker in Brunel

--- brunel_options.py
+++ brunel_options.py
@@ -1,6 +1,7 @@
-from Configurables import Brunel
+# from Configurables import Brunel
 from Configurables import LHCbApp
 from Configurables import L0Conf
+from Gaudi.Configuration import GaudiSequencer
 
 # Set up the databases
 LHCbApp().DDDBtag = 'dddb-20160304'
@@ -9,4 +10,12 @@ LHCbApp().CondDBtag = 'sim-20150716-vc-md100'
 # As we haven't ran Moore
 L0Conf().EnsureKnownTCK = False
 
-Brunel().EvtMax = -1
+# Brunel().EvtMax = 1
+
+# Brunel().Histograms = "Expert"
+
+GaudiSequencer("CheckPatSeq").Members = [
+    "PrChecker",
+    "TrackIPResolutionChecker",
+    "VPClusterMonitor"
+]
--- submit.py
+++ submit.py
@@ -3,26 +3,19 @@ from os.path import join, isdir
 
 # Not needed but keeps flake8 happy
 from Ganga.GPI import (
-    LHCbTask, LHCbTransform, Local, Dirac, prepareGaudiExec, SplitByFiles,
-    TaskChainInput, DaVinci, LocalFile, BKQuery, GaudiExec, DiracFile
+    Local, Dirac, prepareGaudiExec, SplitByFiles, LocalFile, BKQuery,
+    GaudiExec, DiracFile, Job
 )
 
 # Config
 APPCONFIGOPTS = "/afs/cern.ch/lhcb/software/releases/DBASE/AppConfig/v3r312/options"  # NOQA
 BOOKEEPING_PATH = "/MC/Upgrade/Beam7000GeV-Upgrade-MagDown-Nu7.6-Pythia8/Sim09a-Upgrade/27163002/XDIGI"  # NOQA
-
-# Make the LHCbTask
-task = LHCbTask(float=8, name='LHCb hybrid distortion studies')
+RUN_LOCAL = False
 
 # Add Brunel to cmtuser if needed
 brunel_dir = join(os.environ['HOME'], 'cmtuser', 'BrunelDev_v51r1')
 if not isdir(brunel_dir):
     brunel = prepareGaudiExec('Brunel', 'v51r1')
-
-# Add Panoramix to cmtuser if needed
-panoramix_dir = join(os.environ['HOME'], 'cmtuser', 'PanoramixDev_v23r2p2')
-if not isdir(panoramix_dir):
-    brunel = prepareGaudiExec('Panoramix', 'v23r2p2')
 
 # Configure Brunel
 brunel = GaudiExec()
@@ -33,43 +26,29 @@ brunel.options = [
     join(APPCONFIGOPTS, 'Brunel/xdst.py'),
     'brunel_options.py'
 ]
+brunel.extraOpts += '''
+from Configurables import Brunel
+Brunel().EvtMax = {evt_max}
+'''.format(evt_max=[-1, 1][RUN_LOCAL])
 
-# Configure panoramix
-panoramix = GaudiExec()
-panoramix.directory = panoramix_dir
-panoramix.useGaudiRun = False
-panoramix.options = ['panoramix_options.py']
-
-# Make the LHCbTransform for brunel
-brunel_transform = LHCbTransform(
-    name='Reconstruction',
+# Configure the corresponding Job
+brunel_job = Job(
+    name='VP hybrid distortions',
+    comment='Nominal reconstruction test',
     application=brunel,
     splitter=SplitByFiles(filesPerJob=1, ignoremissing=True),
-    outputfiles=[DiracFile('*.xdst')],
+    parallel_submit=True
 )
-task.appendTransform(brunel_transform)
 
-bk_query = BKQuery(path=BOOKEEPING_PATH)
-brunel_transform.addInputData(bk_query.getDataset())
+dataset = BKQuery(path=BOOKEEPING_PATH).getDataset()
 
-# Make the LHCbTransform for panoramix
-panoramix_transform = LHCbTransform(
-    name='Panoramix',
-    application=panoramix,
-    splitter=SplitByFiles(filesPerJob=1),
-    outputfiles=[LocalFile('*.root'), LocalFile('*.png')],
-)
-task.appendTransform(panoramix_transform)
+if RUN_LOCAL:
+    brunel_job.backend = Local()
+    brunel_job.outputfiles = [LocalFile('*.xdst'), LocalFile('*.root')]
+    brunel_job.inputdata = dataset[:8]
+else:
+    brunel_job.backend = Dirac()
+    brunel_job.outputfiles = [DiracFile('*.xdst'), DiracFile('*.root')]
+    brunel_job.inputdata = dataset
 
-d = TaskChainInput()
-d.include_file_mask = ['\.(mdst|dst|gen|digi|sim|xdst)$']
-d.input_trf_id = brunel_transform.getID()
-panoramix_transform.addInputData(d)
-
-for transform in task.transforms:
-    transform.backend = Dirac()
-    # transform.delete_chain_input = True
-    transform.abort_loop_on_submit = False
-    transform.submit_with_threads = True
-    transform.files_per_unit = 10
-task.run()
+brunel_job.submit()

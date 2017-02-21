1ef5a8178e2fe33f6f569144fceb46b713445bba
Christopher Burr <christopher.burr@cern.ch>
2017-02-21 19:02:47 +0100

    Use consistent options when checking positions

--- hybrid-distortions/check_positions/options.py
+++ hybrid-distortions/check_positions/options.py
@@ -1,40 +1,28 @@
-# [SublimeLinter flake8-max-line-length:100]
-from __future__ import print_function
+from Configurables import Brunel
+from Configurables import CondDB
+from Configurables import CondDBAccessSvc
+from Configurables import L0Conf
+from Gaudi.Configuration import GaudiSequencer
 
-from Configurables import Escher, RecSysConf, LHCbApp, RecMoniConf
-from GaudiKernel.ProcessJobOptions import importOptions
-from Configurables import CondDB, CondDBAccessSvc
 
-detectors = ['VP', 'UT', 'FT', 'Magnet', 'Tr']
-
-importOptions('$STDOPTS/PreloadUnits.opts')
-
-LHCbApp().Simulation = True
-LHCbApp().DataType = 'Upgrade'
+Brunel().InputType = "DIGI"
+Brunel().WithMC = True
 CondDB().Upgrade = True
+Brunel().Detectors = ['VP', 'UT', 'FT', 'Rich1Pmt', 'Rich2Pmt', 'Ecal', 'Hcal', 'Muon', 'Magnet', 'Tr']
+Brunel().DataType = "Upgrade"
+Brunel().OutputType = 'XDST'
 
-LHCbApp().Detectors = detectors
+# As we haven't ran Moore
+L0Conf().EnsureKnownTCK = False
 
+GaudiSequencer("CheckPatSeq").Members = [
+    "PrChecker",
+    "TrackIPResolutionChecker",
+    "VPClusterMonitor"
+]
 
-CondDB().addLayer(dbFile='check_positions/DDDB.db', dbName='DDDB')
-CondDB().addLayer(dbFile='check_positions/SIMCOND.db', dbName='SIMCOND')
-alignment_conditions = CondDBAccessSvc('AlignmentConditions')
-alignment_conditions.ConnectionString = 'sqlite_file:./check_positions/Alignment_SIMCOND.db/SIMCOND'
+CondDB().addLayer(dbFile="check_positions/DDDB.db", dbName="DDDB")
+CondDB().addLayer(dbFile="check_positions/SIMCOND.db", dbName="SIMCOND")
+alignment_conditions = CondDBAccessSvc("AlignmentConditions")
+alignment_conditions.ConnectionString = "sqlite_file:check_positions/Alignment_SIMCOND.db/SIMCOND"
 CondDB().addLayer(alignment_conditions)
-
-CondDB().LoadCALIBDB = 'HLT1'
-
-Escher().InputType = "DIGI"
-Escher().Kalman = True
-Escher().Millepede = False
-Escher().EvtMax = -1
-Escher().MoniSequence = []
-Escher().RecoSequence = ["Decoding", "VP", "VELOPIX", "Tr", "Vertex"]
-Escher().WithMC = True
-Escher().Simulation = True
-Escher().DataType = 'Upgrade'
-Escher().Upgrade = True
-RecSysConf().Detectors = detectors
-RecMoniConf().Detectors = detectors
-
-Escher().ExpertTracking = ["kalmanSmoother"]
